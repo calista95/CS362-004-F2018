317a6375479732ad1768c976f2540726488603fe
Calista Wong <wongcal@oregonstate.edu>
2018-10-28 17:28:13 -0700

    unittest3.c

--- dominion/unittest3.c
+++ dominion/unittest3.c
@@ -27,20 +27,32 @@ int main()
 
 	printf("\n\n*****Testing function: supplyCount() : *****\n\n");
 	
-	for (i=0; i<15; i++)
+	for (i=0; i<10; i++)
 	{
 		int maxSupply = supplyCount(i, G); //store the maximum supply available	
-		//int currentSupply = supplyCount(i, G);	//start out with the maximum number of units we have for a card
-		for (j=1; j<maxSupply+2; j++)
+		int currentSupply = supplyCount(i, G);	//start out with the maximum number of units we have for a card
+		for (j=1; j<maxSupply+5; j++)
 		{
 			printf("Buying card %i: ", i);
-			buyCard(i, G); //buy a card	
-			maxSupply--; //decrement supply because we bought a card from the source
-			if (maxSupply == supplyCount(i, G))
-				printf("Pass\n");
-			else
-				printf("Failed\n");
-			
+			int result = buyCard(i, G); //buy a card	
+			currentSupply--; //decrement supply because we bought a card from the source
+			if (result==0) //enough cards in supply
+			{
+				printf("enough cards in supply: ");
+				if (currentSupply == supplyCount(i, G))
+					printf("Pass\n");
+				else
+				
+					printf("Fail\n");
+			}
+			else if (result==-1)//j>maxSupply //not enough cards in supply
+			{
+				printf("not enough cards in supply: ");
+				if (supplyCount(i,G) ==0)
+					printf("Pass\n");
+				else
+					printf("Failed\n");
+			}
 			//make sure that preconditions are satisfied for next testing round
 			G->numBuys++;
 			G->coins = G->coins + 10;
